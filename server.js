a710a2e291a81160e23380e5988f53561542a4a3
Allajah <arayahomuhomu@gmail.com>
2015-04-07 19:17:48 +0900

    Code for geek のstep4まで

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+  res.send('Hello World');
+});
+var server = app.listen(3000,function(){
+  console.log('We have started our server on port 3000');
+});
