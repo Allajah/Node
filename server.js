8f959478d46acc0ce667482d8d3c9a6919a235b2
Allajah <arayahomuhomu@gmail.com>
2015-04-08 16:22:41 +0900

    step5まで終わらせたけどできない

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-app.get('/',function(req,res){
-  res.send('Hello World');
-});
+require('./router/main.js')(app);
+app.set('views',__dirname + 'views');
+app.set('view engine' , 'jade');
+app.engine('html',require('jade').renderFile)
 var server = app.listen(3000,function(){
   console.log('We have started our server on port 3000');
 });
